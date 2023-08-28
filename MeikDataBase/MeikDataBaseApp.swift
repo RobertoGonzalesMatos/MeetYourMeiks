4388a2e7b39ccf0bab0fa616d3b46b9c22275e97
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2023-08-28 16:51:58 -0400

    Initial Commit

---
+++ MeikDataBase/MeikDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeikDataBaseApp.swift
+//  MeikDataBase
+//
+//  Created by Roberto Gonzales on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MeikDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
